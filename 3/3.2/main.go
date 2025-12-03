169f5e1d9e7c1b3541f1c1c546696b4198a8bbaa
victor <thysvic@gmail.com>
2025-12-03 13:43:07 +0100

    Feat: Day 3 Part 2

--- 3/3.2/main.go
+++ 3/3.2/main.go
@@ -1,4 +1,3 @@
-package 
 package main
 
 import (
@@ -17,37 +16,53 @@ func main() {
 	banks := strings.Fields(string(content))
 	fmt.Printf("Loaded in %s\n", banks)
 	for _, bank := range banks {
-		a := analyzeBank(bank)
+		a := analyzeBank(bank, 12)
 		fmt.Printf("The highest combination is : %s\n", a)
 		combs = append(combs, a)
 	}
 	fmt.Printf("The answer is : %d\n", calculateAnswer(combs))
 }
 
-func analyzeBank(bank string) string {
-	var highest_left int
-	var highest_left_i int
-	var highest_right int
-	for i := 0; i < (len(bank) - 1); i++ {
-		digit, err := strconv.Atoi(string(bank[i]))
-		if err != nil {
-			panic(err)
+func analyzeBank(bank string, nB int) string {
+	var values []string
+	var highest_i int
+	for n := 1; n <= nB; n++ {
+		if n == 1 {
+			fmt.Printf("Checking for first digit\n")
+			var highest int
+			for i := 0; i < (len(bank) - (nB - n)); i++ {
+				digit, err := strconv.Atoi(string(bank[i]))
+				if err != nil {
+					panic(err)
+				}
+				if digit > highest {
+					highest = digit
+					highest_i = i
+					fmt.Printf("First highest index : %d\n", highest_i)
+				}
+			}
+			values = append(values, strconv.Itoa(highest))
+		} else {
+			// fmt.Printf("Checking for digit %d, max index : %d\n", n, (len(bank) - (nB - n)))
+			// fmt.Printf("Starting at %d\n", (highest_i + 1))
+			var highest int
+			for i := (highest_i + 1); i < (len(bank) - (nB - n)); i++ {
+				digit, err := strconv.Atoi(string(bank[i]))
+				if err != nil {
+					panic(err)
+				}
+				// fmt.Printf("Checking digit %d\n", digit)
+				if digit > highest {
+					highest = digit
+					highest_i = i
+					fmt.Printf("New highest index : %d\n", highest_i)
+				}
+			}
+			values = append(values, strconv.Itoa(highest))
 		}
-		if digit > highest_left {
-			highest_left = digit
-			highest_left_i = i
-		}
+		fmt.Printf("values : %s\n", values)
 	}
-	for i := (len(bank) - 1); i > highest_left_i; i-- {
-		digit, err := strconv.Atoi(string(bank[i]))
-		if err != nil {
-			panic(err)
-		}
-		if digit > highest_right {
-			highest_right = digit
-		}
-	}
-	return fmt.Sprintf("%d%d", highest_left, highest_right)
+	return strings.Join(values, "")
 }
 
 func calculateAnswer(combs []string) int {
